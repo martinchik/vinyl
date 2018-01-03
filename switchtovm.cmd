49f65fc73f1d6fa650c582e2045ad48d77f7e119
Eugen Martinchik <yauheni.martsinchyk@adform.com>
2018-01-03 12:07:32 +0300

    fix cmds

--- switchtovm.cmd
+++ switchtovm.cmd
@@ -1,5 +1,5 @@
 docker-machine ls
 docker-machine env ubuntu-vinyl
 cmd "$(docker-machine env ubuntu-vinyl)"
-@FOR /f "tokens=*" %i IN ('docker-machine env ubuntu-vinyl') DO @%i
-docker-machine ls
+REM run this text - @FOR /f "tokens=*" %i IN ('docker-machine env ubuntu-vinyl') DO @%i
+REM run this text and view * in active column- docker-machine ls
---
+++ tovm.cmd
@@ -0,0 +1,1 @@
+docker-machine ssh ubuntu-vinyl
